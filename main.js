c9931936c9f950da1ca1d02714c3d2c684fa87c5
Alexey Ospishchev <alexeyosp@gmail.com>
2020-08-20 21:52:31 +0300

    ex 1
    getRow

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
-alert("message");
+const firstRow = 'мама мыла рамуаааааааааааа ';
+const secondRow = 'собака друг человекааааа';
+
+function getRow(firstRow, secondRow) {
+  let num1 = getCharCount(firstRow);
+  let num2 = getCharCount(secondRow);
+    
+    if (num1 > num2)
+    {
+        return firstRow
+    }
+    else {
+       return secondRow
+    }
+    
+}
+
+function getCharCount(row){
+    let count = 0;
+    for (let index = 0; index < row.length; index++) {
+        let char = row.charAt(index);
+                
+        if (char == "а") {
+            count++;
+        }        
+    } 
+    return count
+}
+
+
+
+let str = getRow(firstRow, secondRow)
+
+console.log(str)
